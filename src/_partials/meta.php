94e6528aa20e8f12eaeef7280d888c535db04fea
Dallas Hoffman <dallashoff@gmail.com>
2021-08-10 17:54:55 -0400

    Add icons, manifest, and other metadata

--- src/_partials/meta.php
+++ src/_partials/meta.php
@@ -1,13 +1,19 @@
 <title><?= $title ? $title . ' | ' : '' ?>Caturday Night</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="author" content="Dallas Hoffman">
 <meta name="application-name" content="Caturday Night">
-<meta name="description" content="Every night is Caturday Night">
+<meta name="apple-mobile-web-app-title" content="Caturday Night">
+<meta name="description" content="Every night is Caturday Night.">
+<meta name="theme-color" content="#CF7222">
+<meta name="color-scheme" content="dark">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="manifest" href="/site.webmanifest">
 <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 <link rel="alternate icon" href="/favicon.ico">
 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#cf7222">
-<link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
 <link rel="stylesheet" type="text/css" href="/css/app.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&family=Yusei+Magic&amp;display=swap">
 <script async src="https://kit.fontawesome.com/b520a6ace5.js" crossorigin="anonymous"></script>
